2dd8918dc5f18850adc71fe80681626ba98f0a88
anisia19 <anisia.fort19@gmail.com>
2025-10-27 00:54:23 +0200

    Created a costum hook to fetch the games
    Kept GameGrid component pure and reusable.

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: 'https://api.rawg.io/api',
     params: {
-        key: '359a9dabdcd541cabd210a49dd085b3d'
+        key: ''
     }
 })
