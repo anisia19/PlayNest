4747493d5f9e03d45148ad71831bf5f18efda002
anisia19 <anisia.fort19@gmail.com>
2025-10-30 15:52:11 +0200

    Creating a generetic data hook

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,15 +1,14 @@
-import React from "react";
 import useGenres from "../hooks/useGenres";
 
-const GenredList = () => {
-  const { genres } = useGenres();
+const GenreList = () => {
+  const { data } = useGenres();
   return (
     <ul>
-      {genres.map((genre) => (
+      {data.map((genre) => (
         <li key={genre.id}>{genre.name}</li>
       ))}
     </ul>
   );
 };
 
-export default GenredList;
+export default GenreList;
