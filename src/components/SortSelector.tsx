21b19e45cf68839a1479dac47285c043fc8b6b43
anisia19 <anisia.fort19@gmail.com>
2025-10-31 12:37:15 +0200

    Sort the games

--- src/components/SortSelector.tsx
+++ src/components/SortSelector.tsx
@@ -1,19 +1,36 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from "@chakra-ui/react";
 import { BsChevronDown } from "react-icons/bs";
 
-const SortSelector = () => {
+interface Props {
+  onSelectSortOrder: (sortOrder: string) => void;
+  sortOrder: string;
+}
+
+const SortSelector = ({ onSelectSortOrder, sortOrder }: Props) => {
+  const sortOrders = [
+    { value: "", label: "Relevance" },
+    { value: "-added", label: "Data added" },
+    { value: "-name", label: "Name" },
+    { value: "-relased", label: "Relased Date" },
+    { value: "-metacritic", label: "Popularity" },
+    { value: "-rating", label: "Avarage raiting" },
+  ];
+  const currentSortOrder = sortOrders.find((order) => order.value == sortOrder);
   return (
     <Menu>
       <MenuButton as={Button} rightIcon={<BsChevronDown />}>
-        Order By : Relevance
+        {currentSortOrder?.label || "Relevance"}
       </MenuButton>
       <MenuList>
-        <MenuItem>Relevance</MenuItem>
-        <MenuItem>Data added</MenuItem>
-        <MenuItem>Name</MenuItem>
-        <MenuItem>Relased Date</MenuItem>
-        <MenuItem>Popularity</MenuItem>
-        <MenuItem>Avg raiting</MenuItem>
+        {sortOrders.map((order) => (
+          <MenuItem
+            onClick={() => onSelectSortOrder(order.value)}
+            key={order.value}
+            value={order.value}
+          >
+            {order.label}
+          </MenuItem>
+        ))}
       </MenuList>
     </Menu>
   );
--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -16,10 +16,12 @@ export interface Game {
   metacritic: number;
 }
 
-const useGames = (gameQuery : GameQuery) =>
+const   useGames = (gameQuery : GameQuery) =>
   useData<Game>("/games", {
     params: {
-      genres: gameQuery.genre?.id, platforms: gameQuery.platform?.id
+      genres: gameQuery.genre?.id, 
+      platforms: gameQuery.platform?.id,
+      ordering: gameQuery.sortOrder
     }},
     [gameQuery]);
 
