7fef3576a96672cd7e92c6bbe5a72503329a41b1
anisia19 <anisia.fort19@gmail.com>
2025-10-31 15:53:04 +0200

    Cleanup game cards

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -15,13 +15,15 @@ const GameCard = ({ game }: Props) => {
       <Image src={getCroppedImageUrl(game.background_image)} />
 
       <CardBody>
-        <Heading fontSize="2xl">{game.name}</Heading>
-        <HStack justifyContent="space-between">
-          <PlatformIconList
-            platforms={game.parent_platforms.map((p) => p.platform)}
-          />
-          <CriticScore score={game.metacritic} />
-        </HStack>
+        <Heading fontSize="2xl">
+          {game.name}
+          <HStack justifyContent="space-between" marginBottom={3}>
+            <PlatformIconList
+              platforms={game.parent_platforms.map((p) => p.platform)}
+            />
+            <CriticScore score={game.metacritic} />
+          </HStack>
+        </Heading>
       </CardBody>
     </Card>
   );
